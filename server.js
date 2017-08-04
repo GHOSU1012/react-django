dad558c1ff618c21c1c17b32959cf20448d11602
GHOSU1012 <ghosu1012@gmail.com>
2017-08-04 19:23:29 -0300

    Improving pre-commit and prospector settings

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ new WebpackDevServer(webpack(config), {
   hot: true,
   inline: true,
   historyApiFallback: true,
-  headers: { 'Access-Control-Allow-Origin': '*' }
 }).listen(3000, '0.0.0.0', (err) => {
   if (err) {
     console.log(err);
