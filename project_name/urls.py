fe7db93cd4a60c163531d33088c8bf9cbb8f90ac
GHOSU1012 <ghosu1012@gmail.com>
2017-12-18 13:45:30 -0300

    Updates prospector entry

--- project_name/urls.py
+++ project_name/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls import include, url  # noqa
 from django.contrib import admin
 from django.views.generic import TemplateView
-
 import django_js_reverse.views
 
 
