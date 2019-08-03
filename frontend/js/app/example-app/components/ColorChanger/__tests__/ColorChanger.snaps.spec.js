cfb533a541e20aeb8214cd7b3a1487594f4b78d2
GHOSU1012 <ghosu1012@gmail.com>
2019-08-03 09:49:36 +0000

    chore(package): update enzyme-to-json to version 3.4.0

--- frontend/js/app/example-app/components/ColorChanger/__tests__/ColorChanger.snaps.spec.js
+++ frontend/js/app/example-app/components/ColorChanger/__tests__/ColorChanger.snaps.spec.js
@@ -5,15 +5,20 @@ import ColorChanger from '../ColorChanger';
 
 jest.mock('../../ColorDisplay/ColorDisplay');
 
+
 describe('ColorChanger', () => {
-  test('some title', () => {
-    const wrapper = shallow(<ColorChanger title="This is a test title" />);
+  test('Some title', () => {
+    const wrapper = shallow((
+      <ColorChanger title="This is a test title" />
+    ));
 
     expect(wrapper).toMatchSnapshot();
   });
 
-  test('no title (should use default)', () => {
-    const wrapper = shallow(<ColorChanger />);
+  test('No title (should use default)', () => {
+    const wrapper = shallow((
+      <ColorChanger />
+    ));
 
     expect(wrapper).toMatchSnapshot();
   });
