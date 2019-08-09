4795d2cc11ca8eac24b77bf2606495e6d5d65624
GHOSU1012 <ghosu1012@gmail.com>
2019-08-09 21:57:45 +0000

    fix(package): update webpack-dev-server to version 3.8.0

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,13 +1,4 @@
 // import pages
 import 'bootstrap-includes';
+import 'pages/homePage';
 import '../sass/style.scss';
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as Sentry from '@sentry/browser';
-import App from './App';
-
-// TODO insert sentry dsn
-Sentry.init({ dsn: '' });
-
-ReactDOM.render(<App />, document.getElementById('react-app'));
