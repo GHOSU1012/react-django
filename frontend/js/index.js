017e7e6a1824f6a6c9bd49a8d478baa17472a008
GHOSU1012 <ghosu1012@gmail.com>
2019-08-09 18:03:51 -0300

    Add sentry to front end

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,4 +1,13 @@
 // import pages
 import 'bootstrap-includes';
-import 'pages/homePage';
 import '../sass/style.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
+import App from './App';
+
+// TODO insert sentry dsn
+Sentry.init({ dsn: '' });
+
+ReactDOM.render(<App />, document.getElementById('react-app'));
