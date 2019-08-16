93f2423c742348933d9fc907ab44552ea7620848
GHOSU1012 <ghosu1012@gmail.com>
2019-08-16 16:39:10 -0300

    Remove commands from readme

---
+++ frontend/js/pages/Home.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { hot } from 'react-hot-loader';
+import ColorChanger from '../app/example-app';
+
+const title = 'It really does work! (this section is rendered by React, '
+  + "change the app's name below to test hot reloading)";
+
+
+const Home = () => {
+  const ErrorComponent = null;
+  const [showBugComponent, setShowBugComponent] = useState(false);
+
+  return (
+    <>
+      <ColorChanger title={title} />
+      <button type="button" onClick={() => setShowBugComponent(true)}>Click to test frontend Sentry error handling!</button>
+      {showBugComponent && <ErrorComponent />}
+    </>
+  );
+};
+
+export default hot(module)(Home);
